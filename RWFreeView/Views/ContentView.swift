6717523f9450646133c687e135bf27fa801d8faa
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-10-01 20:01:54 +0300

    add init for ContentView with appearance for navigationBar, also add navigationView to List of episodes

--- RWFreeView/Views/ContentView.swift
+++ RWFreeView/Views/ContentView.swift
@@ -2,10 +2,31 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var store = EpisodeStore()
+
+    init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = UIColor(named: "top-bkgd")
+        appearance.largeTitleTextAttributes =
+        [.foregroundColor: UIColor.white]
+        appearance.titleTextAttributes =
+        [.foregroundColor: UIColor.white]
+        UINavigationBar.appearance().tintColor = .white
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        UISegmentedControl.appearance()
+            .selectedSegmentTintColor = UIColor(named: "list-bkgd")
+    }
+
     
     var body: some View {
-        List(store.episodes, id: \.name) { episode in
-            EpisodeView(episode: episode)
+        NavigationView {
+            List(store.episodes, id: \.name) { episode in
+                NavigationLink(destination: PlayerView(episode: episode)) {
+                    EpisodeView(episode: episode)
+                }
+            }
+            .navigationTitle("Videos")
         }
     }
 }
--- RWFreeView/Views/PlayerView.swift
+++ RWFreeView/Views/PlayerView.swift
@@ -39,6 +39,8 @@ struct PlayerView: View {
                     }
                     Spacer()
                 }
+                .navigationTitle(episode.name)
+                .navigationBarTitleDisplayMode(.inline)
             }
         }
     }
